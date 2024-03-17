5772699e166bfc4bd38fc2375584a15f5faeaa19
Sidney Martins Neto <sydney.martinsn@gmail.com>
2024-03-17 00:16:35 -0400

    adicionar o exemplo_03.py

---
+++ exemplo_03.py
@@ -0,0 +1,13 @@
+# Refatore utilizando variaveis
+
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+nome = input("Digite o seu nome: ")
+qtde_caracteres = len(nome)
+print(qtde_caracteres)
+
+
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+pri_num = int(input("Digite o primeiro valor: "))
+seg_num = int(input("Digite o segungo valor: "))
+resultado = pri_num + seg_num
+print( resultado )
